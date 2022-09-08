87655fffee0dce96ff81870ac4703827c42b8174
Liis <liis.normann@gmail.com>
2022-09-08 13:56:14 +0300

    week 2 - ex 39

---
+++ src/week2/Ex39.java
@@ -0,0 +1,54 @@
+package week2;
+
+public class Ex39 {
+    public static void printStars(int amount) {
+        int times = 1;
+        while (times <= amount) {
+            System.out.println("*");
+            times++;
+        }
+        System.out.println();
+    }
+
+    private static void printSquare(int sideSize) {
+        int row = 1;
+        while (row <= sideSize) {
+            int field = 1;
+            while(field <= sideSize) {
+                System.out.print("* ");
+                field++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+
+    private static void printRectangle(int width, int height) {
+        int row = 1;
+        while (row <= height) {
+            int field = 1;
+            while(field <= width) {
+                System.out.print("* ");
+                field++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+
+
+
+//    public static void main(String[] args) {
+//        printStars(5);
+//        printStars(3);
+//        printStars(9);
+//    }
+
+//    public static void main(String[] args) {
+//        printSquare(4);
+//    }
+
+    public static void main(String[] args) {
+        printRectangle(4, 2);
+    }
+}
