cc5c65aa8f8cc1f6517e6591214e9814c825610c
Liis Normann <liis.normann@gmail.com>
2022-10-24 18:57:36 +0300

    Week 4 - ex 79.2

--- src/week4/Ex79.java
+++ src/week4/Ex79.java
@@ -8,5 +8,7 @@ public class Ex79 {
         stats.addNumber(1);
         stats.addNumber(2);
         System.out.println("Amount: " + stats.amountOfNumbers());
+        System.out.println("Sum: " + stats.sum());
+        System.out.println("Average: " + stats.average());
     }
 }
