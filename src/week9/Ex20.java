d3178fbeebb334ff80b6ea70ae285f2103d2fd21
Liis Normann <liis.normann@gmail.com>
2022-10-25 01:02:24 +0300

    Week 9 - ex 20.4

--- src/week9/Ex20.java
+++ src/week9/Ex20.java
@@ -7,8 +7,6 @@ import week9.measurements.AverageSensor;
 public class Ex20 {
     public static void main(String[] args) {
         Sensor kumpula = new Thermometer();
-        kumpula.on();
-        System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
 
         Sensor kaisaniemi = new Thermometer();
         Sensor helsinkiVantaa = new Thermometer();
@@ -20,5 +18,9 @@ public class Ex20 {
 
         helsinkiArea.on();
         System.out.println("the temperature in Helsinki area is "+helsinkiArea.measure() + " degrees");
+        System.out.println("the temperature in Helsinki area is "+helsinkiArea.measure() + " degrees");
+        System.out.println("the temperature in Helsinki area is "+helsinkiArea.measure() + " degrees");
+
+        System.out.println("readings: "+helsinkiArea.readings());
     }
 }
