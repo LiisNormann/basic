0465584098acc8a5536c2abf2a8d49056384f488
Liis Normann <liis.normann@gmail.com>
2022-10-25 00:47:53 +0300

    Week 9 - ex 18.3 & 18.4

---
+++ src/week9/moving/domain/Box.java
@@ -0,0 +1,39 @@
+package week9.moving.domain;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box implements Thing{
+    private int capacity;
+    private List<Thing> things;
+
+    public Box(int maximumCapacity) {
+        this.capacity = maximumCapacity;
+        things = new ArrayList<Thing>();
+    }
+
+    public boolean addThing(Thing thing){
+        if(thing.getVolume() <= this.capacity) {
+            this.things.add(thing);
+            this.capacity -= thing.getVolume();
+            return true;
+        }
+        return false;
+    }
+
+    public int getVolume(){
+        int thingsCapasity = 0;
+        for (Thing thing:this.things) {
+            thingsCapasity += thing.getVolume();
+        }
+        return thingsCapasity;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder content = new StringBuilder();
+        for (Thing thing:this.things) {
+            content.append("\t").append(thing.toString()).append("\n");
+        }
+        return content.toString();
+    }
+}
