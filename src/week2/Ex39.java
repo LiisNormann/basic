d0eae45309828369067daa3638148e975205aa7e
Liis Normann <liis.normann@gmail.com>
2022-10-24 18:37:41 +0300

    Week 4 - ex 72.2

--- src/week2/Ex39.java
+++ src/week2/Ex39.java
@@ -1,27 +1,27 @@
 package week2;
 
 public class Ex39 {
-    public static void printStars(int amount) {
-        int times = 1;
-        while (times <= amount) {
-            System.out.println("*");
-            times++;
-        }
-        System.out.println();
-    }
+//    public static void printStars(int amount) {
+//        int times = 1;
+//        while (times <= amount) {
+//            System.out.println("*");
+//            times++;
+//        }
+//        System.out.println();
+//    }
 
-    private static void printSquare(int sideSize) {
-        int row = 1;
-        while (row <= sideSize) {
-            int field = 1;
-            while(field <= sideSize) {
-                System.out.print("* ");
-                field++;
-            }
-            System.out.println();
-            row++;
-        }
-    }
+//    private static void printSquare(int sideSize) {
+//        int row = 1;
+//        while (row <= sideSize) {
+//            int field = 1;
+//            while(field <= sideSize) {
+//                System.out.print("* ");
+//                field++;
+//            }
+//            System.out.println();
+//            row++;
+//        }
+//    }
 
     private static void printRectangle(int width, int height) {
         int row = 1;
