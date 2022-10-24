729c407455f60054e5ecc8a9051dd7aad7643926
Liis Normann <liis.normann@gmail.com>
2022-10-24 23:31:22 +0300

    Week 7 - ex 4.2 & 4.3

---
+++ src/week7/Suitcase.java
@@ -0,0 +1,43 @@
+package week7;
+import java.util.ArrayList;
+
+public class Suitcase {
+    private ArrayList<Thing> things;
+    private int weightLimit;
+
+    public Suitcase(int weightLimit) {
+        this.weightLimit = weightLimit;
+        this.things = new ArrayList<Thing>();
+    }
+
+    public void addThing(Thing thing){
+        int weightCurrent = 0;
+        for (Thing currentThing :this.things) {
+            weightCurrent += currentThing.getWeight();
+        }
+        if(weightCurrent + thing.getWeight() <= this.weightLimit) {
+            this.things.add(thing);
+        }
+    }
+
+    @Override
+    public String toString() {
+        int weightCurrent = 0;
+
+        for (Thing currentThing :this.things) {
+            weightCurrent += currentThing.getWeight();
+        }
+
+        String langControl = "";
+
+        if (this.things.size() == 0) {
+            langControl = "empty";
+        } else if (this.things.size() == 1) {
+            langControl = "thing";
+        } else {
+            langControl = "things";
+        }
+
+        return this.things.size() + " " + langControl + " (" + weightCurrent + " kg)";
+    }
+}
