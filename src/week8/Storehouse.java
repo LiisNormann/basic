8c4c2ba6335b886ca07dc9d166b431c40d99734a
Liis Normann <liis.normann@gmail.com>
2022-10-25 00:14:24 +0300

    Week 8 - ex 12.2

--- src/week8/Storehouse.java
+++ src/week8/Storehouse.java
@@ -4,18 +4,34 @@ import java.util.Map;
 
 public class Storehouse {
     private Map<String, Integer> products;
+    private Map<String, Integer> stocks;
 
     public Storehouse() {
         products = new HashMap<String, Integer>();
+        stocks = new HashMap<String, Integer>();
     }
+
     public void addProduct(String product, int price, int stock) {
         this.products.put(product, price);
+        this.stocks.put(product, stock);
     }
+
     public int price(String product) {
-        if (this.products.containsKey(product)){
-            return this.products.get(product);
-        } else {
-            return -99;
+        return this.products.getOrDefault(product, -99);
+    }
+
+    public int stock(String product) {
+        return this.stocks.getOrDefault(product, 0);
+    }
+
+    public boolean take(String product) {
+        if (this.stocks.containsKey(product)){
+            if (this.stocks.get(product) > 0) {
+                this.stocks.put(product, this.stocks.get(product) - 1);
+                return true;
+            }
+            return false;
         }
+        return false;
     }
 }
